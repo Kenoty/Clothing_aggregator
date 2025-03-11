3a0a5c046399770e4b728e9508c0e172740461cf
Vitali Knyrevich <knyrevichvitali@gmail.com>
2025-03-11 14:06:40 +0300

    style fixes

--- src/main/java/com/project/clothingaggregator/controller/UserController.java
+++ src/main/java/com/project/clothingaggregator/controller/UserController.java
@@ -2,16 +2,19 @@ package com.project.clothingaggregator.controller;
 
 import com.project. clothingaggregator.model.User;
 import com.project. clothingaggregator.service.UserService;
+import java.util.List;
 import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/users")
 public class UserController {
 
     private final UserService userService;
@@ -21,14 +24,14 @@ public class UserController {
         this.userService = userService;
     }
 
-    @GetMapping("/user")
-    public ResponseEntity<User> getUserInfoByName(@RequestParam(value = "name") String name) {
-        Optional<User> user = userService.findByName(name);
-        return user.map(ResponseEntity::ok)
-                .orElseGet(() -> ResponseEntity.status(HttpStatus.NOT_FOUND).body(null));
+    @GetMapping
+    public ResponseEntity<List<User>> getUsersByName(@RequestParam(value = "name") String name) {
+        List<User> users = userService.findByName(name);
+        return users.isEmpty() ? ResponseEntity.status(HttpStatus.NOT_FOUND)
+                .body(null) : ResponseEntity.ok(users);
     }
 
-    @GetMapping("/user/{id}")
+    @GetMapping("/{id}")
     public ResponseEntity<User> getUserInfoById(@PathVariable int id) {
         Optional<User> user = userService.getUserById(id);
         return user.map(ResponseEntity::ok)
--- src/main/java/com/project/clothingaggregator/service/UserService.java
+++ src/main/java/com/project/clothingaggregator/service/UserService.java
@@ -3,8 +3,10 @@ package com.project.clothingaggregator.service;
 import com.project.clothingaggregator.model.User;
 import java.time.LocalDate;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.Optional;
+import java.util.stream.Collectors;
 import org.springframework.stereotype.Service;
 
 
@@ -21,12 +23,13 @@ public class UserService {
         userDatabase.put(1, new User(1, "Igor", LocalDate.of(1990, 1, 1), "igor@example.com"));
         userDatabase.put(2, new User(2, "Bob", LocalDate.of(1985, 5, 15), "bob@mail.ru"));
         userDatabase.put(3, new User(3, "John", LocalDate.of(2004, 7, 25), "john@gmail.com"));
+        userDatabase.put(4, new User(4, "Igor", LocalDate.of(2000, 3, 18), "example@example.com"));
     }
 
-    public Optional<User> findByName(String name) {
+    public List<User> findByName(String name) {
         return userDatabase.values().stream()
                 .filter(u -> u.getName().equalsIgnoreCase(name))
-                .findFirst();
+                .collect(Collectors.toList());
     }
 
     public Optional<User> getUserById(int id) {
