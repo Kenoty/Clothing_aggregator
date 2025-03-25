47370c27a2d85d338597c6c77faa1a9370f1b5c8
Vitali Knyrevich <knyrevichvitali@gmail.com>
2025-03-25 05:03:18 +0300

    exceptions was added

---
+++ src/main/java/com/project/clothingaggregator/exception/GlobalExceptionHandler.java
@@ -0,0 +1,21 @@
+package com.project.clothingaggregator.exception;
+
+import org.springframework.data.crossstore.ChangeSetPersister;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler extends RuntimeException {
+
+    @ExceptionHandler(ChangeSetPersister.NotFoundException.class)
+    public ResponseEntity<String> handleNotFound(NotFoundException ex) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
+    }
+
+    @ExceptionHandler(IllegalStateException.class)
+    public ResponseEntity<String> handleConflict(IllegalStateException ex) {
+        return ResponseEntity.status(HttpStatus.CONFLICT).body(ex.getMessage());
+    }
+}
